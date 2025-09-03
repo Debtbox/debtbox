2e650637a7c23e77c09f6e98eeefcaf47b621999
Mostafa Fathi <mostafafathielsebaey@gmail.com>
2025-09-03 14:40:57 +0300

    chore: update homepage URL, adjust Vite base path, and simplify Router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename={process.env.NODE_ENV === 'production' ? '/debtbox' : '/'}>
+    <Router>
       <div className="w-screen h-screen text-center items-center justify-center flex">
         <h1 className="text-4xl font-bold text-teal-900">DebtBox</h1>
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/debtbox/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
