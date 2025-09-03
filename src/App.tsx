f14929430ef6f0b1a8bceb3a4232f1012bc602f7
Mostafa Fathi <mostafafathielsebaey@gmail.com>
2025-09-03 14:46:23 +0300

    style: change header color from teal to red for improved visibility

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <Router>
       <div className="w-screen h-screen text-center items-center justify-center flex">
-        <h1 className="text-4xl font-bold text-teal-900">DebtBox</h1>
+        <h1 className="text-4xl font-bold text-red-700">DebtBox</h1>
       </div>
     </Router>
   );
