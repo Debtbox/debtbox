113e0d7e5e78329fb563a88427c15964bd36fe64
Mostafa Fathi <mostafafathielsebaey@gmail.com>
2026-02-04 20:26:26 +0200

    refactor: remove NafathCallback component and its route from authentication flow to simplify code structure

--- src/features/auth/routes/index.tsx
+++ src/features/auth/routes/index.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import { Login } from '../views/Login';
 import { SignUp } from '../views/SignUp';
 import { ForgotPassword } from '../views/ForgotPassword';
-import NafathCallback from '../views/NafathCallback';
 
 export const AuthRoutes = () => {
   return (
@@ -11,7 +10,6 @@ export const AuthRoutes = () => {
         <Route path="login" element={<Login />} />
         <Route path="sign-up" element={<SignUp />} />
         <Route path="forgot-password" element={<ForgotPassword />} />
-        <Route path="nafath-callback" element={<NafathCallback />} />
       </Route>
     </Routes>
   );
