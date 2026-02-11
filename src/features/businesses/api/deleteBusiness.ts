8e16929485eaf4686c212137580fb2f37cd40f16
Mostafa Fathi <mostafafathielsebaey@gmail.com>
2026-02-11 02:07:39 +0200

    feat: enhance deleteBusiness API to include Accept-Language header for improved localization support

--- src/features/businesses/api/deleteBusiness.ts
+++ src/features/businesses/api/deleteBusiness.ts
@@ -2,9 +2,14 @@ import { axios } from '@/lib/axios';
 import { useMutation } from '@tanstack/react-query';
 import type { MutationConfig } from '@/lib/react-query';
 import type { ApiError } from '@/types/ApiError';
+import { getLanguageFromCookie } from '@/utils/getLanguageFromCookies';
 
 export const deleteBusiness = ({ businessId }: { businessId: string }) => {
-  return axios.delete(`/business/delete-business?businessId=${businessId}`);
+  return axios.delete(`/business/delete-business?businessId=${businessId}`, {
+    headers: {
+      'Accept-Language': getLanguageFromCookie(),
+    },
+  });
 };
 
 type UseDeleteBusinessOptions = {
